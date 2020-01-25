af542147045416c2cf3a1a7f9ecf128100f31fd5
imanshafiei <imanshafiei@ymail.com>
2020-01-26 02:02:59 +0330

    supplier fix.

--- Supplier.py
+++ Supplier.py
@@ -1,17 +1,14 @@
 from Invoices import Invoices
-from InvoicePay import InvoicePay
-from InvoiceExpense import InvoiceExpense
-from InvoicePurchase import InvoicePurchase
 from BalanceStrategy import DebtorBalanceStrategy, CreditorBalanceStrategy
 
 class Supplier:
-    def __init__(self,name, phonenumber):
+    def __init__(self,name, phone_number):
         self.name = name
-        self.phonenumber = phonenumber
+        self.phone_number = phone_number
 
     @staticmethod
-    def create_supplier(name, phonenumber):
-        return Supplier(name, phonenumber)
+    def create_supplier(name, phone_number):
+        return Supplier(name, phone_number)
 
     def calculate_balance(self,balance_strategy_obj):
         my_invoices = Invoices.get_invoices_per_supplier(self)
@@ -20,18 +17,4 @@ class Supplier:
     def calculate_total_balance(self):
         debtor_amount = self.calculate_balance(DebtorBalanceStrategy)
         creditor_amount = self.calculate_balance(CreditorBalanceStrategy)
-
-    def get_balancestrategyobj(self):
-        return self.balance_strategy_obj
-
-    def get_invoiceobj(self):
-        return self.invoice_obj
-
-    def get_inventoryobj(self):
-        return self.inventroy_obj
-
-    def set_balancestrategyobj(self,balance_strategy_obj):
-        self.balance_strategy_obj = balance_strategy_obj
-
-    def set_invoiceobj(self, invoice_obj):
-        self.invoice_obj = invoice_obj
+        return debtor_amount - creditor_amount
